8d774ce43719784fdf387acb36f84f11166a2764
NILAY-10000 <87806888+NILAY-10000@users.noreply.github.com>
2024-02-27 17:17:14 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
+var song1="";
+var song2="";
 function preload(){
-
+    song1=loadSound("blinding_lights.mp3");
+    song2=loadSound("boulevard_of_broken_dreams.mp3");
 }
 function setup(){
     canvas=createCanvas(550,500);
     canvas.position(550,150);
+    video=createCapture(VIDEO);
+    video.hide();
 }
 function draw(){
-    
+    image(video,0,0,550,500);
 }
